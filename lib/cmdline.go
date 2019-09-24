c9db753a21ad442ddd0fe92de5062da8cad23921
medasz <601364951@qq.com>
2019-09-24 16:49:21 +0800

    update

---
+++ lib/cmdline.go
@@ -0,0 +1,7 @@
+package lib
+
+
+func Parse_args(){
+
+}
+
--- subDomainsBrute.go
+++ subDomainsBrute.go
@@ -1,28 +1,30 @@
-package subDomainsBrute
+package main
 
 import (
 	"fmt"
 	"github.com/miekg/dns"
 	"log"
-	"net"
 )
 
 func main(){
-	config,err:=dns.ClientConfigFromFile("./dns_server.txt")
-	if err!=nil{
-		log.Fatal(err)
-	}
+
+
+
+
+	//config,err:=dns.ClientConfigFromFile("./dns_server.txt")
+	//if err!=nil{
+	//	log.Fatal(err)
+	//}
 	//构造客户端
 	c:=new(dns.Client)
 	//构造dns报文
 	m:=new(dns.Msg)
 	m.SetQuestion(dns.Fqdn("www.imun.edu.cn"), dns.TypeA)
 	m.RecursionDesired = true
-	r,_,err:=c.Exchange(m,net.JoinHostPort(config.Servers[0],config.Port))
+	//r,_,err:=c.Exchange(m,net.JoinHostPort(config.Servers[0],config.Port))
+	r,_,err:=c.Exchange(m,"8.8.8.8:53")
 	if err!=nil{
 		log.Fatal(err)
 	}
-	fmt.Println(config)
 	fmt.Println(r.Answer[0].(*dns.A).A)
-	//fmt.Println(r.Answer[0].(*dns.CNAME))
 }
